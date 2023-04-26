2a334a2a8813237a5e56d1758228cb511ebda2e5
Marina Bibichenko <108667532+MarinaUser@users.noreply.github.com>
2023-04-26 15:58:04 +0300

    connection database

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+const mongoose = require('mongoose')
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+
+const DB_HOST = "mongodb+srv://Maryna:QTCRPeimV5JLhKK7@cluster0.0voe4js.mongodb.net/db-contacts"
+
+mongoose.set("strictQuery", true);
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+		app.listen(3000, () => {
+			console.log("Successful connection database");
+		});
+	})
+	.catch(error => {
+		console.log(error.message);
+		process.exit(1);
+	});
